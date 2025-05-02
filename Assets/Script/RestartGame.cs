2d7a23536a2457a95692af02ac4e3bed0d512a71
atip pongphimai <atip.pong@bumail.net>
2025-05-02 22:35:05 +0700

    ทำฉากหน้า Main Menu

--- Assets/Script/RestartGame.cs
+++ Assets/Script/RestartGame.cs
@@ -5,7 +5,6 @@ public class RestartGame : MonoBehaviour
 {
     public void ReloadScene()
     {
-        // โหลดซีนปัจจุบันใหม่
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);
     }
