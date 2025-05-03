cba8e79385c0cad14a746fb81033dd8565484f20
Naruechit <naruechit.pana@bumail.net>
2025-05-03 23:57:02 +0700

    Enemy damage& Player health

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -2,25 +2,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    /*public EnemyHealth enemyHealth;
+    public int damage = 1;*/
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //delete object 
         Destroy(gameObject,2f);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     void OnCollisionEnter2D(Collision2D target)
     {
-        if (target.gameObject.tag == "Enemy")
+        if (target.gameObject.CompareTag("Enemy"))
         {
             Destroy(gameObject);
             Destroy(target.gameObject);
+            //enemyHealth.takedamage(damage);
         }
     }
 }
