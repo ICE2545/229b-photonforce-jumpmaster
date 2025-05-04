75fa399b13c94b046841f0cd4a2655eb74d4c615
Poomnapat <poomnapat.tumr@bumail.net>
2025-05-04 21:55:16 +0700

    Final Pushhh

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -4,6 +4,7 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health;
     public int maxHealth = 3;
+
     void Start()
     {
         health = maxHealth;
@@ -15,6 +16,17 @@ public class PlayerHealth : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {            
+            Debug.Log("Player Died!");
+
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Rigidbody2D>().simulated = false;
         }
     }
 }
